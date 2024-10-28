7fc798d747e3f4da7bf9499d06eb59d7f667b519
Jose E. Roman <jroman@dsic.upv.es>
2024-10-28 09:43:32 +0100

    Increase patchlevel to 3.22.1

--- src/binding/slepc4py/src/slepc4py/__init__.py
+++ src/binding/slepc4py/src/slepc4py/__init__.py
@@ -21,7 +21,7 @@ a large, sparse, rectangular matrix.
 """
 
 __author__  = 'Lisandro Dalcin'
-__version__ = '3.22.0'
+__version__ = '3.22.1'
 __credits__ = 'SLEPc Team <slepc-maint@upv.es>'
 
 # -----------------------------------------------------------------------------
