a91d12759fff4160128aff5f301525f1ae70751d
Jose E. Roman <jroman@dsic.upv.es>
2026-01-04 09:47:06 +0100

    Remove unneded python import

--- config/packages/slepc4py.py
+++ config/packages/slepc4py.py
@@ -8,7 +8,6 @@
 #  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #
 
-from __future__ import print_function
 import sys, os, log, package
 
 class Slepc4py(package.Package):
